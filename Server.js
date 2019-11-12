6ff0a57a82b1bec6d7fe21f9840c6f6726729f3c
maxilvert <44968480+maxilvert@users.noreply.github.com>
2019-11-12 16:09:10 +0100

    Server.js

--- Server.js
+++ Server.js
@@ -2,71 +2,152 @@
 //La variable express nous permettra d'utiliser les fonctionnalités du module Express. 
 
 var express = require('express');
-
+var bodyParser = require("body-parser")
 var upload = require("multer")()
 
 // Nous définissons ici les paramètres du serveur.
 var hostname = 'localhost'; 
 var port = 3000; 
- 
+
 // Nous créons un objet de type Express. 
 var app = express(); 
-
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : ''
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'cesi_user'
 });
 
-connection.connect();
-connection.end();
-
+connection.connect(function(err) {
+	if (err) throw err
+		console.log('You are now connected...')
+})
 
 //POST
-app.post("/pisc",upload.none(),function(req,res){
-	console.log("insertion de la piscine", req.body["longueur"], " en base...")
-	//console.log(req)
-	//connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA']);
-    res.json({message : "Ajoute une nouvelle piscine à la liste", methode : req.method});
+app.post("/:table",upload.none(),function(req,res,field){
+	var nom_bool=false;
+	var prenom_bool= false;
+	var id_centre_bool=false;
+	var mail_bool=false;
+	var mdp_bool=false;
+	console.log("hello")
+	var obj = Object.entries(req.body)
+	var TabChamp=[]
+	obj.forEach(function(element) {
+		if (element[0]=="Nom") {
+			nom_bool=true;
+			TabChamp[0] = element[1];
+			console.log("tetet");
+		}
+		else if (element[0]=="Prenom") {
+			prenom_bool=true;
+			TabChamp[1] = element[1];
+		}
+		else if (element[0]=="id_centre") {
+			id_centre_bool=true;
+			TabChamp[2] = element[1];
+		}
+		else if (element[0]=="mail") {
+			mail_bool=true
+			TabChamp[3] = element[1];
+		}
+		else if (element[0]=="mdp") {
+			mdp_bool=true
+			TabChamp[4] = element[1];
+		}
+	})
+
+	if (nom_bool && prenom_bool && id_centre_bool && mail_bool && mdp_bool) {
+		var sql = 'INSERT INTO '+ req.params["table"] +' (Nom, Prenom ,id_centre, mail, mdp) values (?,?,?,?,?)';
+		var champ = [TabChamp[0],TabChamp[1],TabChamp[2],TabChamp[3],TabChamp[4]];
+		sql = mysql.format(sql, champ);
+		connection.query(sql, function(err,result,req,field)
+		{
+			if (err)
+			{
+				console.error(err.stack);
+			}
+			else{
+				res.json({message : result, methode : "post"});
+			};
+
+		})
+	}
+
 })
 
- 
-//Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
-//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes. 
-var myRouter = express.Router(); 
- 
-// Je vous rappelle notre route (/piscines).  
-myRouter.route('/piscines/:id')
-// J'implémente les méthodes GET, PUT, UPDATE et DELETE
-// GET
-.get(function(req,res){ 
-	
-	//connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text');
-	console.log("recherche de la piscine", req.params["id"], " en base...")
-	res.json({message : "Liste toutes les piscines de Lille Métropole", methode : req.method});
 
+app.delete("/:table/:id",upload.none(),function(req,res,field){
+	var sql= 'DELETE FROM '+ req.params["table"] +' WHERE id = '+ req.params["id"]
+	connection.query(sql, function(err,result){
+		if (err) {
+			console.error(err.stack);
+		}
+		else{
+			console.log(result)
+			res.json({message : result , methode : req.method});
+		}
+	});
 })
-//POST
-.post(function(req,res){
-	console.log("insertion de la piscine", req.body, " en base...")
-	console.log(req)
-	//connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA']);
-    res.json({message : "Ajoute une nouvelle piscine à la liste", methode : req.method});
-})
-//PUT
-.put(function(req,res){
 
-      res.json({message : "Mise à jour des informations d'une piscine dans la liste", methode : req.method});
+//Permet avec la méthode get de demander d'avoir la liste des élèves en le demandant dans l'url
+app.get("/:table",upload.none(),function(req,res,field){
+	console.log("Voici la liste des éléve ", req.body["Nom"],req.body["Prenom"],req.body["Age"], " en base...")
+	connection.query('SELECT*FROM '+ req.params["table"], function(err,result){
+		if (err) {
+			console.error(err.stack);
+		}
+		else{
+			res.json({message : result , methode : req.method});
+		}
+	});
 })
-//DELETE
-.delete(function(req,res){ 
-res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});  
-}); 
- 
-// Nous demandons à l'application d'utiliser notre routeur
-app.use(myRouter);  
- 
+
+app.get('/:table/:id',upload.none(),function(req,res,field){
+	var queryStringGET ='SELECT*FROM '+ req.params["table"] +' WHERE Id='+ mysql.escape(req.params["id"]);
+	connection.query(queryStringGET, function(err,result){
+		if (err) {
+			console.error(err.stack);
+		}
+		else{
+			console.log(result)
+			res.json({message : result , methode : req.method});
+		}
+	});
+})
+
+app.put("/:table/:id",upload.none(),function(req,res,field){
+	var table=req.params["table"]
+	var id = req.params["id"]
+	var objPut = Object.entries(req.body)
+	var TabPut=[]
+	var i = 0
+	objPut.forEach(function(element) {
+		if (typeof element[1]=='string') {
+			element[1] = '"' + element[1] + '"'
+		}
+		TabPut[i] = element.join("=")
+		i++
+	})
+	var str = TabPut.join(', ')
+	console.log(str)
+	var sql= "UPDATE "+table+" SET "+str+ " WHERE id = "+ id
+	connection.query(sql, function(err,result){
+		if (err) {
+			console.error(err.stack);
+		}
+		else{
+			console.log(result)
+			res.json({message : result , methode : req.method});
+		}
+	});
+})
+
 // Démarrer le serveur 
 app.listen(port, hostname, function(){
 	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
